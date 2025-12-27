a69152ac65d546e0af3fca3dfac588aa3df05f0d
pro-coconut <gez004989@gmail.com>
2025-12-27 22:16:47 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-// main.js
-let debounce=null;
+const editor = CodeMirror(document.getElementById("editor"), {
+  value:
+`print("Executor ready")
 
-function run(code){
-  try{
-    if(window.__SAFE__ && code.includes("getgenv")) throw "Blocked by Safe mode";
-    window.__runLuau(code);
-  }catch(e){
-    log("Error: "+e);
-  }
+-- loadstring example:
+-- loadstring("https://raw.githubusercontent.com/user/repo/main/test.lua")()`,
+  mode: "lua",
+  lineNumbers: true
+});
+window.editor = editor;
+
+function clearConsole(){
+  document.getElementById("console").textContent="";
+}
+function clearUI(){
+  document.getElementById("ui").innerHTML="";
 }
 
+/* SAFE REALTIME */
+let timer=null;
 editor.on("change",()=>{
-  if(!rt.classList.contains("on")) return;
-  clearTimeout(debounce);
-  debounce=setTimeout(()=>{
-    document.getElementById("console").textContent="";
-    window.__SAFE__=safe.classList.contains("on");
-    run(editor.getValue());
-  },300);
+  clearTimeout(timer);
+  timer=setTimeout(()=>{
+    clearConsole();
+    clearUI();
+    runLuau(editor.getValue());
+  },400); // safe delay chống lag
 });
